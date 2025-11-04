ed3a35a29fcff17bac04cc28c9f01fe73d03993c
sambhav291 <sambhavmagotra009@gmail.com>
2025-11-04 22:23:46 +0530

    bugs 2

--- src/app/components/ContactAnimation.tsx
+++ src/app/components/ContactAnimation.tsx
@@ -88,9 +88,7 @@ function FloatingStars() {
       <bufferGeometry>
         <bufferAttribute
           attach="attributes-position"
-          count={count}
-          array={positions}
-          itemSize={3}
+          args={[positions, 3]}
         />
       </bufferGeometry>
       <pointsMaterial 
